08a3f93b34ea07608d30c2e980868a9d84b477e5
WayneJr <40968414+WayneJr@users.noreply.github.com>
2020-10-16 23:18:07 -0400

    Created new form and done media queries also added model for meals. Added Read, new and Create routes and static directory

--- api/router.js
+++ api/router.js
@@ -1,9 +1,35 @@
+const Meal = require('../models/meal');
+
+
 module.exports = app => {
     app.get('/', (req, res) => {
         res.render('landing');
     });
 
+    
+    // Read Route
     app.get('/meals', (req, res) => {
-        res.render('meals');
+        Meal.find({}, (err, meals) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('index', {meals: meals});
+            }
+        })
+    });
+
+    app.get('/meals/new', (req, res) => {
+        res.render('new');
+    })
+
+    // Create route
+    app.post('/meals', (req, res) => {
+        Meal.create(req.body.meal, (err, meal) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect('/meals');
+            }
+        });
     });
 }
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
 
 router(app);
